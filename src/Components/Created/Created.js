761c76fa50bb2df3d87a23df44f175df33e920f5
violetacf <violeta@cique.es>
2024-11-14 01:57:53 +0100

    Add flaticon for use rights

--- src/Components/Created/Created.js
+++ src/Components/Created/Created.js
@@ -1,23 +1,28 @@
+import { useContext } from 'react';
+import { SpanishModeContext } from '../../Context/SpanishModeContext';
 import './Created.css';
 import ReactImg from '../../Images/react_logo.png';
 
 export default function CreatedBy() {
+  const { spanishMode } = useContext(SpanishModeContext);
+
   return (
     <div className="created-container">
-      <img
-        className="react-logo"
-        src={ReactImg}
-        alt="React logo"
-      ></img>
+      <img className="react-logo" src={ReactImg} alt="React logo"></img>
       <p>
-        Designed & Developed by
-        <a href="https://violeta.cique.es/">Violeta Cique</a>- 2024
+        {spanishMode
+          ? 'Diseñado y Desarrollado por'
+          : 'Designed & Developed by'}
+        {''}
+        <a href="https://violeta.cique.es/">Violeta Cique</a> - 2024
       </p>
-      <img
-        className="react-logo"
-        src={ReactImg}
-        alt="React logo"
-      ></img>
+      <img className="react-logo" src={ReactImg} alt="React logo"></img>
+      <p>
+        {spanishMode ? 'Iconos de' : 'Icons from'}
+        {''}
+        <a href="https://www.flaticon.com/">Flaticon</a>
+      </p>
+      <img className="react-logo" src={ReactImg} alt="React logo"></img>
     </div>
   );
 }
